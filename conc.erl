d4954bfacf250a0f2a7df390f1b71bedc60fc277
sauuls <24saul24@gmail.com>
2023-05-18 15:40:55 -0600

    conc lunes15

--- conc.erl
+++ conc.erl
@@ -1,13 +1,28 @@
 -module(conc).
--export(inicio/0, di_algo/2).
+-export([inicio/0, di_algo/2, areas/0]).
 
 di_algo(_, 0) ->
-    hecho;
+	hecho;
 di_algo(Que, Veces) ->
-    io:format("Que: ~p~n", [Que]),
-    di_algo(Que, Veces - 1).
+	io:format("~p~n", [Que]),
+	di_algo(Que, Veces - 1).
 
 inicio() ->
-    spawn(conc, di_algo, [hola, 3]),
-    spawn(conc, di_algo, [adios, 3]),
+	spawn(conc, di_algo, [hola, 3]),
+	spawn(conc, di_algo, [adios, 3]),
     spawn(conc, di_algo, [bye, 3]).
+    
+areas() ->
+  receive
+    {rectangulo, Anchura, Altura} ->
+	io:format("Area del rectangulo = ~p~n" ,[Anchura * Altura]),
+	areas();
+    {circulo, R} ->
+	io:format("Area del circulo = ~p~n" , [3.14159 * R * R]),
+	areas();
+    termina -> 
+        io:format("Termine de ejecutarme~n");
+    Otro ->
+	io:format("Desconozco el area del ~p ~n" ,[Otro]),
+	areas()
+  end.
